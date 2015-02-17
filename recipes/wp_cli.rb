e55d2f5303916a0cb3cf9e17198f6c2a7cc8ffc5
derbandn <derbandn@yahoo.com>
2015-02-17 16:15:22 +0300

    wp-cli

--- recipes/wp_cli.rb
+++ recipes/wp_cli.rb
@@ -20,14 +20,18 @@ remote_file "wp-cli.phar" do
     source 'https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar'
     mode "0744"
     path "/usr/local/bin/wp"
-    not_if { ::File.exists?("/usr/local/bin/wp") }
+    not_if { ::File.exists?("/usr/local/bin/wp") 
+      
+      execute "wp-cli" do
+         command "wp core install --allow-root && wp theme activate twentyfifteen --allow-root"
+         cwd "/var/www"
+    
+        end
+      
+      }
 end
 
-execute "wp-cli" do
-    command "wp core install --allow-root && wp theme activate twentyfifteen --allow-root"
-    cwd "/var/www"
-    
-end
+
 
 
 
